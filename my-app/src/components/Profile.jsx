91683fd747925dc028123a8e466d5026f908932d
artem1606 <53149823+artem1606@users.noreply.github.com>
2020-01-22 03:52:04 +0300

    added file

---
+++ my-app/src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div><img src="https://www.w3schools.com/howto/img_snow_wide.jpg" alt=""/></div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                MyPosts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 1</div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
